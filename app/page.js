c0b02a9f6b5e8a8b8aa8848918d9fa7db65cc84c
OndrejHj04 <ondra.hajku@seznam.cz>
2023-07-01 18:19:39 +0200

    header and hero init

--- app/page.js
+++ app/page.js
@@ -1,15 +1,25 @@
 import { getProjects } from "@/sanity/sanity-utils";
+import "./styles.scss";
+import Image from "next/image";
 
 export default async function Home() {
   const data = await getProjects();
 
   return (
-    <div>
-      Perfect website!!
-      <div>
-        {data.map((project, i) => (
-          <p key={i}>{project.name}</p>
-        ))}
+    <div className="body">
+      <div className="banner">
+        <h2 className="text md:text-3xl text-2xl">
+          Lorem ipsum dolor sit amet 20%!
+        </h2>
+      </div>
+      <div className="hero">
+        <div className="container">
+          <h1 className="title md:text-6xl text-5xl">
+            Lorem ipsum dolor sit amet
+          </h1>
+          <img src="rectangleLeft.png" className="reactangle-left" />
+          <img src="reactangleRight.png" className="reactangle-right" />
+        </div>
       </div>
     </div>
   );
