7e3c83c12940d7db8680d417dc627b3d6a4a85dd
OndrejHj04 <ondra.hajku@seznam.cz>
2023-07-02 17:16:27 +0200

    version 0.1

--- app/page.js
+++ app/page.js
@@ -6,25 +6,10 @@ export default async function Home() {
 
   return (
     <div className="body">
-      <div className="banner">
-        <h2 className="text md:text-3xl text-2xl">
-          Lorem ipsum dolor sit amet 20%!
-        </h2>
-      </div>
-      <div className="hero">
-        <div className="container"></div>
-      </div>
-      <div className="content">
-        <div className="container">
-          <div></div>
-          <img src="space.png" />
-        </div>
-      </div>
-      <div className="waves">
-        <img src="waveTop.png" className="wave-top" />
-        <div className="container11"></div>
-        <img src="waveBottom.png" className="wave-left" />
-      </div>
+      <div className="banner"></div>
+      <div className="hero"></div>
+      <div className="section"></div>
+      <div className="waves"></div>
     </div>
   );
 }
