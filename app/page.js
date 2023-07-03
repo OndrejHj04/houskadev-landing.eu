eed4bdd40e75e5e2d512d111517542f95386c9a9
OndrejHj04 <ondra.hajku@seznam.cz>
2023-07-03 16:28:08 +0200

    version 0.5

--- app/page.js
+++ app/page.js
@@ -6,17 +6,33 @@ export default async function Home() {
 
   return (
     <div className="body">
-      <div className="banner"></div>
+      <div className="banner">
+        <img src="close.png" className="close" />
+        <h3 className="title">Lorem ipsum dolor sit amet 20%!</h3>
+      </div>
       <div className="hero">
-        <div className="title"></div>
+        <div className="title-box">
+          <h1 className="title">Lorem ipsum dolor sit amet</h1>
+        </div>
         <div className="rectangles">
           <div className="rect-left"></div>
-          <div className="rect-right"></div>
+          <div className="rect-right">
+            <h4 className="text">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit!!!{" "}
+            </h4>
+          </div>
         </div>
       </div>
       <div className="section">
         <div className="content">
-          <div className="text-container"></div>
+          <div className="text-container">
+            <h2 className="title">Lorem ipsum dolor sit amet</h2>
+            <p className="parag">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In sem
+              justo, commodo ut, suscipit at, pharetra vitae, orci. Phasellus
+              enim erat, vestibulum vel, aliquam a, posuere eu, velit.
+            </p>
+          </div>
           <div className="image-container">
             <img src="space.png" />
           </div>
@@ -26,10 +42,16 @@ export default async function Home() {
         <img src="waveTop.png" className="wave-top" />
         <div className="content">
           <div className="graph-container">
-            <div className="graph-title"></div>
+            <div className="graph-title">
+              <h3 className="title">Lorem ipsum</h3>
+              <p className="parag">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Integer rutrum, orci vestibulum ullamcorper ultricies.
+              </p>
+            </div>
             <div className="graphs">
-              <img src="chartLeft.png" className="graph"/>
-              <img src="chartRight.png" className="graph"/>
+              <img src="chartLeft.png" className="graph" />
+              <img src="chartRight.png" className="graph" />
             </div>
           </div>
         </div>
